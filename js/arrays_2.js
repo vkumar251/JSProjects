2db4fdbbbfc67ac1b5f604d55b054f7c98e0fd44
vkumar251 <vk.chowdhury25@gmail.com>
2025-07-07 11:59:18 +0100

    Updated Project

--- js/arrays_2.js
+++ js/arrays_2.js
@@ -6,15 +6,15 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function listColours()
 {
-    let text = "";
+    let text = ""; // setup empty string to store the list items
     let length = colours.length;
-    text = text + "<h2>Colours:</h2>";
+    text = text + "<h2>Colours:</h2>"; // heading
 
-    for (x = 0; x < length; x++)
+    for (x = 0; x < length; x++) // loop through each array index
     {
         text = text + colours[x] + "<br>";
     }
-    document.getElementById("colours-list").innerHTML = text;
+    document.getElementById("colours-list").innerHTML = text; // locate element with id "colours-list" and update
 }
 function listSubjects()
 {
@@ -56,4 +56,17 @@ function listNumbers()
         text = text + numbers[i] + "<br>";
     }
     document.getElementById("numbers-list").innerHTML = text;
+}
+function listNumber5()
+{
+    let text = "";
+    let length = numbers.length;
+    const output = document.getElementById("numbers-list-5");
+
+    text = text + "<h2>Number five:</h2>";
+    text = text + "This is the fifth number of the array. <br>";
+    text = text + numbers[4] + "<br>";
+    
+    output.innerHTML = text;
+    output.style.color = "green";
 }
